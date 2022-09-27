57d3e07098d3007d4c0cc693f8b5bd774c96531e
njasi <nick@jasinski3.com>
2022-09-26 18:38:58 -0700

    base socket setup just for spam

--- src/server/socket/index.js
+++ src/server/socket/index.js
@@ -1,5 +1,11 @@
 module.exports = (io) => {
   io.on("connection", (socket) => {
+    console.log("Connected new socket client:\t", socket.id);
+
     socket.on("disconnect", () => {});
+
+    socket.on("norman", (msg) => {
+      socket.broadcast.emit("serverNorman", msg);
+    });
   });
 };
