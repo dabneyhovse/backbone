5f00cb54af58a2cba990d4fc6eb3415301d892de
Melissa Foster <melissaemilyfoster@gmail.com>
2024-09-11 19:39:37 -0700

    Removed passport sessions and configured express-openid-connect

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,10 @@
 /**
  * Author:	Nick Jasinski
- * Date:		2022-08-19
+ * Date:		2024-09-11
  *
  * Main express appsetup
  *  pg db sync
- *  passport session setup
+ *  OIDC setup
  *  add auth, api, static routes
  *  send index html
  *  endware for error handling
@@ -17,7 +17,8 @@ const express = require("express");
 const morgan = require("morgan");
 const compression = require("compression");
 const session = require("express-session");
-const passport = require("passport");
+// const passport = require("passport");
+const { auth } = require("express-openid-connect");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const db = require("./db");
 const sessionStore = new SequelizeStore({ db });
@@ -34,18 +35,22 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
+if (process.env.NODE_ENV !== "development") {
+  app.set('trust proxy', '127.0.0.1');
+}
+
 // passport registration
-passport.serializeUser((user, done) => done(null, user.id));
-// passport deserialization using oor db
-passport.deserializeUser(async (id, done) => {
-  try {
-    // TODO restrict the data that loads into passport
-    const user = await db.models.user.findByPk(id);
-    done(null, user);
-  } catch (err) {
-    done(err);
-  }
-});
+// passport.serializeUser((user, done) => done(null, user.id));
+// // passport deserialization using oor db
+// passport.deserializeUser(async (id, done) => {
+//   try {
+//     // TODO restrict the data that loads into passport
+//     const user = await db.models.user.findByPk(id);
+//     done(null, user);
+//   } catch (err) {
+//     done(err);
+//   }
+// });
 
 const createApp = () => {
   // logging middleware
@@ -58,21 +63,33 @@ const createApp = () => {
   // compression middleware
   app.use(compression());
 
-  // session middleware with passport
+  // // session middleware with passport
+  // app.use(
+  //   session({
+  //     secret: process.env.SESSION_SECRET || "oof",
+  //     store: sessionStore,
+  //     resave: false,
+  //     saveUninitialized: true,
+  //   })
+  // );
+  // app.use(passport.initialize());
+  // app.use(passport.session());
+
+  // // auth and api routes
+
   app.use(
-    session({
-      secret: process.env.SESSION_SECRET || "oof",
-      store: sessionStore,
-      resave: false,
-      saveUninitialized: true,
+    auth({
+      authRequired: false,
+      idpLogout: true,
+      attemptSilentLogin: true,
+      session: {
+        secret: process.env.SESSION_SECRET,
+        store: sessionStore,
+      },
     })
   );
-  app.use(passport.initialize());
-  app.use(passport.session());
 
-  // auth and api routes
-
-  app.use("/auth", require("./auth"));
+  // app.use("/auth", require("./auth"));
   app.use("/api", require("./api"));
   app.use("/", require("./static"));
 
