563fa3dde1ccc9330f3d7132a4f940cdfa56a74b
Melissa Foster <melissaemilyfoster@gmail.com>
2024-09-16 22:46:54 -0700

    Disabled old API - some API routes may return eventually but most are replaced by Keycloak APIs

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -1,68 +1,68 @@
-/**
- * Author:	Nick Jasinski
- * Date:		2022-08-15
- *
- * Api index file
- *
- */
+// /**
+//  * Author:	Nick Jasinski
+//  * Date:		2022-08-15
+//  *
+//  * Api index file
+//  *
+//  */
 
-const { populateApiKey } = require("./middleware/apikey");
+// const { populateApiKey } = require("./middleware/apikey");
 
-const router = require("express").Router();
+// const router = require("express").Router();
 
-/**
- * attaches all services apis to the router
- *
- * @param {express router} apiRouter
- */
-function attachServices(apiRouter) {
-  console.log("Attaching service api routes...");
-  /**
-   * only need these json files loaded while
-   * loading this so seems prudent to do this
-   *
-   * kinda cursed but i dont want to fix it yet srry
-   */
-  const { serviceConfigs } = require("../../services");
-  const allServices = serviceConfigs;
+// /**
+//  * attaches all services apis to the router
+//  *
+//  * @param {express router} apiRouter
+//  */
+// function attachServices(apiRouter) {
+//   console.log("Attaching service api routes...");
+//   /**
+//    * only need these json files loaded while
+//    * loading this so seems prudent to do this
+//    *
+//    * kinda cursed but i dont want to fix it yet srry
+//    */
+//   const { serviceConfigs } = require("../../services");
+//   const allServices = serviceConfigs;
 
-  // TODO: possibly differientate between Express and Api in the future
+//   // TODO: possibly differientate between Express and Api in the future
 
-  let count = 0;
-  allServices.forEach((service) => {
-    if (service.importExpress) {
-      apiRouter.use(
-        `/${service.route}`,
-        require(`${service.moduleName}/Express`)
-      );
-      count++;
-      console.log(
-        `\t[${service.name}]:\tattached api routes at /api/${service.route}/`
-      );
-    } else {
-      console.log(`\t[${service.name}]:\tdoes not have api routes`);
-    }
-  });
-  console.log(`Attached ${count} service api Route(s)\n`);
-}
+//   let count = 0;
+//   allServices.forEach((service) => {
+//     if (service.importExpress) {
+//       apiRouter.use(
+//         `/${service.route}`,
+//         require(`${service.moduleName}/Express`)
+//       );
+//       count++;
+//       console.log(
+//         `\t[${service.name}]:\tattached api routes at /api/${service.route}/`
+//       );
+//     } else {
+//       console.log(`\t[${service.name}]:\tdoes not have api routes`);
+//     }
+//   });
+//   console.log(`Attached ${count} service api Route(s)\n`);
+// }
 
-router.use(populateApiKey);
-// attach base backbone routes
-router.use("/users", require("./users"));
-router.use("/affiliations", require("./affiliations"));
-router.use("/groups", require("./groups"));
-router.use("/keys", require("./keys"));
-router.use("/scopes", require("./scopes"));
-router.use("/dbux", require("./dbux"));
-router.use("/telegram", require("./telegram"));
+// router.use(populateApiKey);
+// // attach base backbone routes
+// router.use("/users", require("./users"));
+// router.use("/affiliations", require("./affiliations"));
+// router.use("/groups", require("./groups"));
+// router.use("/keys", require("./keys"));
+// router.use("/scopes", require("./scopes"));
+// router.use("/dbux", require("./dbux"));
+// router.use("/telegram", require("./telegram"));
 
-// attach routes for services
-attachServices(router);
+// // attach routes for services
+// attachServices(router);
 
-router.use((req, res, next) => {
-  const error = new Error("API Route Not Found");
-  error.status = 404;
-  next(error);
-});
+// router.use((req, res, next) => {
+//   const error = new Error("API Route Not Found");
+//   error.status = 404;
+//   next(error);
+// });
 
-module.exports = router;
+// module.exports = router;
--- src/server/api/telegram/index.js
+++ src/server/api/telegram/index.js
@@ -1,99 +1,99 @@
-const router = require("express").Router();
-const bot = require("./bot");
-const { User } = require("../../db/models");
-const { hasApiScope } = require("module-middleware");
-module.exports = router;
+// const router = require("express").Router();
+// const bot = require("./bot");
+// const { User } = require("../../db/models");
+// const { hasApiScope } = require("module-middleware");
+// module.exports = router;
 
-/**
- * Set the bot API endpoint for webhook,
- * or start in polling mode if NODE_ENV is develop
- *
- * POST /api/telegram/TELEGRAM_WEBHOOK_SECRET
- */
-async function launchBot() {
-  if ((process.env.NODE_ENV = "development")) {
-    // polling instead in dev mode
-    bot.launch();
-    console.log("Started telegram bot in polling mode.\n");
-  } else {
-    botListener = await bot.createWebhook({
-      domain: process.env.TELEGRAM_WEBHOOK_DOMAIN,
-      port: 443,
-      hookPath: `/api/telegram/${process.env.TELEGRAM_WEBHOOK_SECRET}`,
-    });
+// /**
+//  * Set the bot API endpoint for webhook,
+//  * or start in polling mode if NODE_ENV is develop
+//  *
+//  * POST /api/telegram/TELEGRAM_WEBHOOK_SECRET
+//  */
+// async function launchBot() {
+//   if ((process.env.NODE_ENV = "development")) {
+//     // polling instead in dev mode
+//     bot.launch();
+//     console.log("Started telegram bot in polling mode.\n");
+//   } else {
+//     botListener = await bot.createWebhook({
+//       domain: process.env.TELEGRAM_WEBHOOK_DOMAIN,
+//       port: 443,
+//       hookPath: `/api/telegram/${process.env.TELEGRAM_WEBHOOK_SECRET}`,
+//     });
 
-    router.use(`/${process.env.TELEGRAM_WEBHOOK_SECRET}`, botListener);
-    console.log("Started telegram bot in webhook mode.\n");
-  }
-}
+//     router.use(`/${process.env.TELEGRAM_WEBHOOK_SECRET}`, botListener);
+//     console.log("Started telegram bot in webhook mode.\n");
+//   }
+// }
 
-launchBot();
+// launchBot();
 
-/**
- * these are seperate from the user cause they are telegram id based, meant for
- * bots
- * */
+// /**
+//  * these are seperate from the user cause they are telegram id based, meant for
+//  * bots
+//  * */
 
-/**
- * GET /api/telegram/isdarb
- *
- * req.query.telegram_id:
- *    the telegram id you want to check
- *
- * req.query.api_key:
- *    api key with scope "telegram-isdarb"
- *
- * returns json
- *    .id : backbone internal user id
- *    .firstname : firstname that the user has set in bio
- *    .lastname : lastname that the user has set in bio
- *    .username : user's username
- *    .profile : json object with more details
- *        { photo, room, bio }
- */
-router.get("/user", hasApiScope("telegram-user"), async (req, res, next) => {
-  try {
-    const user = await User.findOne({
-      where: { telegram_id: req.query.telegram_id },
-      attributes: ["id", "firstName", "lastName", "username", "profile"],
-    });
+// /**
+//  * GET /api/telegram/isdarb
+//  *
+//  * req.query.telegram_id:
+//  *    the telegram id you want to check
+//  *
+//  * req.query.api_key:
+//  *    api key with scope "telegram-isdarb"
+//  *
+//  * returns json
+//  *    .id : backbone internal user id
+//  *    .firstname : firstname that the user has set in bio
+//  *    .lastname : lastname that the user has set in bio
+//  *    .username : user's username
+//  *    .profile : json object with more details
+//  *        { photo, room, bio }
+//  */
+// router.get("/user", hasApiScope("telegram-user"), async (req, res, next) => {
+//   try {
+//     const user = await User.findOne({
+//       where: { telegram_id: req.query.telegram_id },
+//       attributes: ["id", "firstName", "lastName", "username", "profile"],
+//     });
 
-    if (user == null) {
-      err = new Error("User not found");
-      err.status = 404;
-      throw err;
-    }
+//     if (user == null) {
+//       err = new Error("User not found");
+//       err.status = 404;
+//       throw err;
+//     }
 
-    res.json(user).status(200);
-  } catch (error) {
-    next(error);
-  }
-});
+//     res.json(user).status(200);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
-/**
- * GET /api/telegram/isdarb
- *
- * req.query.telegram_id:
- *    the telegram id you want to check
- *
- * req.query.api_key:
- *    api key with scope "telegram-isdarb"
- *
- * returns json
- *    .darb : true if darb & telegram account linked, false otherwise
- */
-router.get(
-  "/isdarb",
-  hasApiScope("telegram-isdarb"),
-  async (req, res, next) => {
-    try {
-      const user = await User.findOne({
-        where: { telegram_id: req.query.telegram_id },
-      });
+// /**
+//  * GET /api/telegram/isdarb
+//  *
+//  * req.query.telegram_id:
+//  *    the telegram id you want to check
+//  *
+//  * req.query.api_key:
+//  *    api key with scope "telegram-isdarb"
+//  *
+//  * returns json
+//  *    .darb : true if darb & telegram account linked, false otherwise
+//  */
+// router.get(
+//   "/isdarb",
+//   hasApiScope("telegram-isdarb"),
+//   async (req, res, next) => {
+//     try {
+//       const user = await User.findOne({
+//         where: { telegram_id: req.query.telegram_id },
+//       });
 
-      res.json({ darb: user == null }).status(200);
-    } catch (error) {
-      next(error);
-    }
-  }
-);
+//       res.json({ darb: user == null }).status(200);
+//     } catch (error) {
+//       next(error);
+//     }
+//   }
+// );
