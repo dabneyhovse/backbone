dba9c4d222ae44387d730cb4a60e5faa44e3176b
Melissa Foster <melissaemilyfoster@gmail.com>
2024-09-18 12:20:24 -0700

    Added keycloak API axios instance and mTLSAgent

--- src/server/index.js
+++ src/server/index.js
@@ -21,16 +21,12 @@ const session = require("express-session");
 const { auth } = require("express-openid-connect");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const db = require("./db");
+const mTLSAgent = require("./auth/mTLSAgent");
 const sessionStore = new SequelizeStore({ db });
 const PORT = process.env.PORT || 8080;
 const app = express();
 const socketio = require("socket.io");
-const { Agent } = require("https");
-const { readFileSync } = require("fs");
-const mTLSAgent = new Agent({
-  key: readFileSync(process.env.CLIENT_KEY_PATH, "utf8"),
-  cert: readFileSync(process.env.CLIENT_CERT_PATH, "utf8"),
-});
+
 module.exports = app;
 
 if (process.env.NODE_ENV === "test") {
