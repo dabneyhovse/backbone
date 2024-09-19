1227b2bd16aa0d53123d1cd67a7d11c373cc67e2
Melissa Foster <melissaemilyfoster@gmail.com>
2024-09-19 11:29:06 -0700

    Moved to separate module

--- src/server/auth/keycloak.js
+++
@@ -1,9 +0,0 @@
-const mTLSAgent = require("./mTLSAgent");
-const { Axios } = require("axios");
-
-const keycloak = Axios.create({
-    baseURL: process.env.ISSUER_BASE_URL,
-    httpsAgent: mTLSAgent,
-})
-
-module.exports = keycloak;
