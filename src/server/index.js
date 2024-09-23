b68260fd2802a9be3ae153478b3e51705ac87ae5
Melissa Foster <melissaemilyfoster@gmail.com>
2024-09-23 00:53:47 -0700

    Imported better

--- src/server/index.js
+++ src/server/index.js
@@ -21,7 +21,7 @@ const session = require("express-session");
 const { auth } = require("express-openid-connect");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const db = require("./db");
-import mTLSAgent from "module-keycloak/mTLSAgent";
+const { default: mTLSAgent } = await import("module-keycloak/mTLSAgent");
 const sessionStore = new SequelizeStore({ db });
 const PORT = process.env.PORT || 8080;
 const app = express();
