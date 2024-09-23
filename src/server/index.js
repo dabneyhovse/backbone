ec4f69299d2568dc6d2c98473708fd6e366ed26d
Melissa Foster <melissaemilyfoster@gmail.com>
2024-09-23 00:37:11 -0700

    Switched to ES import

--- src/server/index.js
+++ src/server/index.js
@@ -21,7 +21,7 @@ const session = require("express-session");
 const { auth } = require("express-openid-connect");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const db = require("./db");
-const { mTLSAgent } = require("module-keycloak/mTLSAgent");
+import mTLSAgent from "module-keycloak/mTLSAgent";
 const sessionStore = new SequelizeStore({ db });
 const PORT = process.env.PORT || 8080;
 const app = express();
